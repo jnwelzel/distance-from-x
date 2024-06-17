90d21636c925ec8812d6f4830018ea08fda1fb50
Jon Welzel <jnwelzel@gmail.com>
2024-06-17 09:37:52 +0200

    Fixed export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,9 +4,7 @@ export { CoordinatesForm } from "./CoordinatesForm/CoordinatesForm";
 export { FormInput } from "./FormInput/FormInput";
 export { FORM_INPUT_NAMES } from "./CoordinatesForm/state";
 export { BUTTON_NAMES, initialState } from "./CoordinatesForm/state";
-export {
-  coordinatesFormReducer,
-  FormActionTypes,
-} from "./CoordinatesForm/reducer";
+export { coordinatesFormReducer } from "./CoordinatesForm/reducer";
 export type { IFormState } from "./CoordinatesForm/state";
 export type { IButtons } from "./CoordinatesForm/state";
+export { FormActionTypes } from "./CoordinatesForm/actions";
